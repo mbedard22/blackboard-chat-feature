f2040a03fd8a0eb905c14d598432917eab8aadfa
gabriel <vegabito1112@gmail.com>
2022-11-17 21:16:36 -0500

    functinality for the second page

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,8 @@ routes = Blueprint("routes", __name__)
 def index():
 
     return render_template("dashboard.html", title="Dashboard")
+
+@routes.route('/example-class')
+def exampleClass():
+
+    return render_template("class.html", title="Home Page -- COMP 4110")
